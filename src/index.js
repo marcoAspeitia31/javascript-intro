321fae272ced627bd760478ad73c31f79a089317
Marco Aspeitia <maspeitia.31@gmail.com>
2022-11-27 21:11:06 -0600

    Clase 9 introducción a promesas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import { getHeroesById, getHeroesByOwner } from './bases/08-imports-exports'
+console.log('Inicio')
 
-console.log( getHeroesById(1) )
-console.log( getHeroesByOwner( 'Marvel' ) )
+new Promise( (resolve, reject) => {
+
+    console.log('cuerpo de la promesa')
+    resolve('Promesa resuelta')
+
+} )
+.then( console.log ) //Esto es igual que la siguiente línea, el primer parámetro del callback pasa como primer parámetro de la siguiente función
+.catch( err => console.error( err ) )
+
+console.log('Fin')
