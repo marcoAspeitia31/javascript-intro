9bba27ab32a2dd123e9dcec49efdda7ab6f1b6a8
Marco Aspeitia <maspeitia.31@gmail.com>
2022-11-27 18:05:31 -0600

    Clase 5 arrow functions

--- src/index.js
+++ src/index.js
@@ -1,20 +1,63 @@
-const array = [1,2,3,4]
-//También se pueden crear como new Array() pero es poco común
+/*
+function saludar( xyz ) {
+    return `Hola mundo ${xyz}`
+}
 
-array.push(5)
+const saludar = function( xyz ){
+    return `Hola mundo ${xyz}`
+}
 
-const array2 = [ ...array ]
+*/
+//Forma natural de arrow functions
+const saludar = ( nombre ) => {
+    return `Hola mundo ${nombre}`
+}
+//Forma simplificada
+const saludarNew = ( nombre ) => `Hola ${nombre}`
+//Nota, si sólo se tiene un argumento se pueden omitir los paréntesis en estos
 
-array2.push(6)
+//Forma simplificada
+const saludarPorDefecto = ( nombre = 'Marco' ) => `Hola ${nombre}`
 
-//Cuando ocupamos métodos propios del prototipo como map generan una nueva referencia
-//de lo que retornan como arreglo
-const array3 = array2.map( (n) => {
-    return n * 2
-} )
+const nombre = 'Tony'
 
-array3.push(14)
+console.log( saludar(nombre) )
+console.log( saludarNew(nombre) )
+console.log( saludarPorDefecto() )
 
-console.log(array)
-console.log(array2)
-console.log(array3)
+//Return de arrow function a return implícito
+const getUser = () => {
+    return {
+        uid: 'ABC123',
+        username: 'Marco31'
+    }
+}
+const getUserImplicit = () => ({
+    uid: 'ABC123',
+    username: 'Marco31'
+})
+
+console.log( getUser() )
+console.log( getUserImplicit() )
+
+const heroes = [
+    {
+        id: 1,
+        name: 'Batman'
+    },
+    {
+        id: 2,
+        name: 'Spiderman'
+    }
+]
+
+const even = ( item ) => {
+    if( item.id == 1 ) {
+        return true
+    }
+}
+
+const even2 = ( item ) => item.id == 2
+
+console.log( heroes.some( even ) )
+console.log( heroes.some( even2 ) )
