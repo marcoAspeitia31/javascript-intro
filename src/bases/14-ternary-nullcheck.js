6d7ff9b7fa456072f2ddbc18b705fcc8bafb8535
Marco Aspeitia <maspeitia.31@gmail.com>
2022-11-28 21:01:08 -0600

    Clase 14 null check y operadores ternarios

---
+++ src/bases/14-ternary-nullcheck.js
@@ -0,0 +1,13 @@
+let firstName
+let lastName = 'Aspeitia'
+
+// Null check
+// console.log(`${ firstName || 'No firstName'} ${ lastName || 'No lastName' }`)
+
+const isActive = true
+
+// Ternarios
+const message = ( isActive === true ) ? 'Activo' : 'Inactivo'
+
+console.log(message)
+
